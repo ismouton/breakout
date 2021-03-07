341044de4ef2145702c6e4d2aec8649844dd3545
Shane Mouton <github@ismouton.org>
2021-03-07 06:59:35 -0500

    Add documentation

--- src/Context/index.js
+++ src/Context/index.js
@@ -3,6 +3,9 @@ import Video from "./Video";
 import Mouse from "./Mouse";
 import Keyboard from "./Keyboard";
 
+/**
+ * Default video options passed to video context constructor.
+ */
 const defaultVideoOptions = {
 	scale: 4,
 	width: 320,
@@ -19,6 +22,9 @@ class Context {
 		this.mouse = new Mouse({ canvas, video: this.video });
 		this.keyboard = new Keyboard({ canvas });
 
+		/**
+		 * Global variables used by application.
+		 */
 		this.state = {
 			blocksDestroyed: 0,
 			score: 0,
